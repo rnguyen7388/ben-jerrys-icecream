29cdca1cf34ba797b73d66217eb75c7686e26908
Ray <rnguyen7388@gmail.com>
2020-07-28 21:24:06 -0500

    Made index and flavor templates

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
 
-const app = expres();
+const app = express();
 const PORT = 8080;
 
 app.engine('handlebars', exphbs());
@@ -14,15 +14,21 @@ var icecreams = [
     {name: 'banana', price: 1, awesomeness: 1},
     {name: 'greentea', price: 5, awesomeness: 7},
     {name: 'jawbreakers', price: 6, awesomeness: 2},
-    { name: "pistachio", price: 11, awesomeness: 15 }
+    {name: "pistachio", price: 11, awesomeness: 15}
   ];
 
-  app.get(`/icecream/:name`, (req, res) => {
+  app.get(`/`, (req, res) => {
+      res.render("index")
+  })
 
+  app.get(`/icecreams/:name`, (req, res) => {
+    const targetFlavor = req.params.name
+    const found = icecreams.find(flavor => flavor.name === targetFlavor)
+    res.render("flavor", found)
   })
 
   app.get(`/icecreams`, (req, res) => {
-      
+    // Loop over all the ice creams and display them all to the user
 })
 
   app.listen(PORT, () => console.log(`🚀 on http://localhost:${PORT}`))
